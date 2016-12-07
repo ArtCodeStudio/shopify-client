0dc14922f34317b925bc7745ffc8f0b6ea612922
Pascal Garber <pascal@jumplink.eu>
2016-12-07 13:15:24 -0500

    own object for videos

--- video-client.js
+++ video-client.js
@@ -2,7 +2,11 @@ if(!shopifyApp) {
     var shopifyApp = {};
 }
 
-shopifyApp.initVideo = function (shopName, firebaseIdToken, callback) {
+if(!shopifyApp.video) {
+    shopifyApp.video = {};
+}
+
+shopifyApp.video.init = function (shopName, firebaseIdToken, callback) {
     console.log("initApi", shopName, firebaseIdToken);
     $.getJSON( config.shopifyApp.microserviceVideoBaseUrl+"/init/"+window.config.appName+"/"+shopName+"/"+firebaseIdToken+"?callback=?", function( res ) {
         console.log("greate you are signed in to the microservice-video:", res);  
@@ -11,7 +15,7 @@ shopifyApp.initVideo = function (shopName, firebaseIdToken, callback) {
 };
 
 // Beispiel, bitte umändern
-shopifyApp.api = function (resource, method, params, callback) {
+shopifyApp.video.api = function (resource, method, params, callback) {
     $.getJSON( config.shopifyApp.microserviceApiBaseUrl+"/api/"+window.config.appName+"/"+window.config.shopifyApp.shopName+"/"+resource+"/"+method+"?callback=?", function( result ) {
         console.log("api:", result);  
         callback(null, result);
